825fbb9b3a1cb0de79748ce1c7d8a46d674908fb
Alex1124 <summersnow.dawn@gmail.com>
2022-12-28 18:26:40 +0800

    Update model.py
    way2 work
    
    Signed-off-by: Alex1124 <summersnow.dawn@gmail.com>

--- app/model.py
+++ app/model.py
@@ -1,25 +1,25 @@
 ## coding: UTF-8#
-from xgboost import XGBClassifier
-
-
-def predict(input):
-    xgbmodel_iris = XGBClassifier()                         ##創立xgb分類物件
-    xgbmodel_iris.load_model('app/model/xgb_iris.txt')     ##xgb物件載入參數 xgb_iris.json xgb_iris.txt
-    pred = xgbmodel_iris.predict(input)[0]
-    print(pred)
-    return pred
-
-
-# #way2
-# import pickle
-# import gzip
-
-# # 載入Model
-# with gzip.open('app/model/xgb_iris.pgz', 'r') as f:
-#     xgboostModel = pickle.load(f)
+# from xgboost import XGBClassifier
 
 
 # def predict(input):
-#     pred=xgboostModel.predict(input)[0]
+#     xgbmodel_iris = XGBClassifier()                         ##創立xgb分類物件
+#     xgbmodel_iris.load_model('app/model/xgb_iris.txt')     ##xgb物件載入參數 xgb_iris.json xgb_iris.txt
+#     pred = xgbmodel_iris.predict(input)[0]
 #     print(pred)
 #     return pred
+
+
+#way2
+import pickle
+import gzip
+
+# 載入Model
+with gzip.open('app/model/xgb_iris.pgz', 'r') as f:
+    xgboostModel = pickle.load(f)
+
+
+def predict(input):
+    pred=xgboostModel.predict(input)[0]
+    print(pred)
+    return pred
