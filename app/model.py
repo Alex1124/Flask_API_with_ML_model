7ff3617a8098b09ebac52c64cb7aa9d8296b878c
Alex1124 <summersnow.dawn@gmail.com>
2022-12-28 16:19:44 +0800

    Update model.py
    use pickle way
    
    Signed-off-by: Alex1124 <summersnow.dawn@gmail.com>

--- app/model.py
+++ app/model.py
@@ -1,11 +1,23 @@
 ## coding: UTF-8#
-from xgboost import XGBClassifier
+# from xgboost import XGBClassifier
+
+
+# def predict(input):
+#     xgbmodel_iris = XGBClassifier()                         ##創立xgb分類物件
+#     xgbmodel_iris.load_model('app/model/xgb_iris.json')     ##xgb物件載入參數
+#     pred = xgbmodel_iris.predict(input)[0]
+#     print(pred)
+#     return pred
+
+import pickle
+import gzip
+
+# 載入Model
+with gzip.open('app/model/xgb_iris.pgz', 'r') as f:
+    xgboostModel = pickle.load(f)
 
 
 def predict(input):
-    xgbmodel_iris = XGBClassifier()                         ##創立xgb分類物件
-    xgbmodel_iris.load_model('app/model/xgb_iris.json')     ##xgb物件載入參數
-    pred = xgbmodel_iris.predict(input)[0]
+    pred=xgboostModel.predict(input)[0]
     print(pred)
     return pred
-
