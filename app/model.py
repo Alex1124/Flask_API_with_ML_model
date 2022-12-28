1a83b553dcd2fa7075ecbb1c13c3c95588eb14cb
Alex1124 <summersnow.dawn@gmail.com>
2022-12-28 08:31:36 +0800

    Update model.py

--- app/model.py
+++ app/model.py
@@ -4,7 +4,7 @@ from xgboost import XGBClassifier
 
 def predict(input):
     xgbmodel_iris = XGBClassifier()                         ##創立xgb分類物件
-    xgbmodel_iris.load_model(r'app/model/xgb_iris.json')     ##xgb物件載入參數
+    xgbmodel_iris.load_model('app/model/xgb_iris.json')     ##xgb物件載入參數
     pred = xgbmodel_iris.predict(input)[0]
     print(pred)
     return pred
